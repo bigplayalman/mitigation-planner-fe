a1c0c38df56740fe3029b04baeb5be2ab8aea147
Almas Barzhaxynov <almas@oomzie.com>
2022-09-24 11:22:29 -0400

    reusing mechanics form for better setting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MechanicFormComponent } from './components/mechanic-form/mechanic-form.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MechanicFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/planner/planner.module.ts
+++ src/app/planner/planner.module.ts
@@ -1,22 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 
 import { PlannerRoutingModule } from './planner-routing.module';
 import { PlannerComponent } from './planner.component';
-import { MechanicFormComponent } from './mechanic-form/mechanic-form.component';
 
 
 @NgModule({
   declarations: [
     PlannerComponent,
-    MechanicFormComponent
   ],
   imports: [
     CommonModule,
     PlannerRoutingModule,
-    MatInputModule,
     MatButtonModule
   ]
 })
