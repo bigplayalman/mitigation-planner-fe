58e8c58d2980f91110327bbf25ba418e24cfd5b1
Almas Barzhaxynov <almas@oomzie.com>
2022-09-24 08:21:56 -0400

    added basic structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'planner', loadChildren: () => import('./planner/planner.module').then(m => m.PlannerModule) },
+  { path: '', redirectTo: '/planner', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
